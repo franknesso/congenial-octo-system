609196f9f10aefc9eb55510fd48a112dfa2849c6
Artem <158818111+veiiras@users.noreply.github.com>
2024-11-01 21:37:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import streamlit as st
 
 API_KEY = '21027f31-6334-4abe-98c0-7bede6caf0c5'
 
-if 'labr_token_balance' not in st.session_state:
-    st.session_state['labr_token_balance'] = None
-    st.session_state['wtrx_token_balance'] = None
-    st.session_state['wtrx_price_in_usd'] = None
-    st.session_state['latest_labr_price'] = None
-
-@st.cache_data(ttl=60)
+@st.cache_resource(ttl=60)
 def fetch_data():
     try:
         response = requests.get(
@@ -18,13 +12,13 @@ def fetch_data():
 
         if response.status_code != 200:
             st.error(f"Ошибка: не удалось получить данные. Код состояния: {response.status_code}")
-            return
+            return None, None, None, None
 
         response_in_json = response.json()
 
         if 'data' not in response_in_json:
             st.error("Ошибка: ключ 'data' не найден в ответе API.")
-            return
+            return None, None, None, None
 
         labr_token_balance = next((token['quantity'] for token in response_in_json['data'] if token['tokenAbbr'] == 'LABR'), None)
         wtrx_token_balance = next((token['quantity'] for token in response_in_json['data'] if token['tokenAbbr'] == 'WTRX'), None)
@@ -35,25 +29,24 @@ def fetch_data():
 
         if response.status_code != 200:
             st.error(f"Ошибка: не удалось получить цену WTRX. Код состояния: {response.status_code}")
-            return
+            return None, None, None, None
 
         response_in_json = response.json()
 
         if 'trc20_tokens' not in response_in_json or not response_in_json['trc20_tokens']:
             st.error("Ошибка: данные 'trc20_tokens' не найдены в ответе API.")
-            return
+            return None, None, None, None
 
         wtrx_price_in_usd = response_in_json['trc20_tokens'][0]['market_info'].get('priceInUsd', None)
 
         if labr_token_balance and wtrx_token_balance and wtrx_price_in_usd:
-            st.session_state['labr_token_balance'] = labr_token_balance
-            st.session_state['wtrx_token_balance'] = wtrx_token_balance
-            st.session_state['wtrx_price_in_usd'] = wtrx_price_in_usd
-            st.session_state['latest_labr_price'] = wtrx_token_balance / labr_token_balance * wtrx_price_in_usd
+            latest_labr_price = wtrx_token_balance / labr_token_balance * wtrx_price_in_usd
+            return labr_token_balance, wtrx_token_balance, wtrx_price_in_usd, latest_labr_price
         else:
             st.error("Ошибка: не удалось получить все необходимые данные.")
     except Exception as e:
         st.error(f'Ошибка при загрузке данных: {e}')
+    return None, None, None, None
 
 def calculate_slippage(token_x_balance: int, token_y_balance: int, delta_x: int, delta_y: int) -> float:
     return (delta_x / delta_y) - (token_x_balance / token_y_balance)
@@ -76,37 +69,36 @@ def calculate_price_after_selling(token_x_balance: int, token_y_balance: int, de
 
 def main():
     st.title("LABR Token Price Calculator")
-    
-    fetch_data()
-    print(st.session_state['latest_labr_price'], st.session_state['labr_token_balance'], st.session_state['wtrx_token_balance'], st.session_state['wtrx_price_in_usd'])
 
-    if not all([st.session_state['labr_token_balance'], st.session_state['wtrx_token_balance'], st.session_state['wtrx_price_in_usd'], st.session_state['latest_labr_price']]):
+    labr_token_balance, wtrx_token_balance, wtrx_price_in_usd, latest_labr_price = fetch_data()
+
+    if not all([labr_token_balance, wtrx_token_balance, wtrx_price_in_usd, latest_labr_price]):
         st.error("Данные временно недоступны. Попробуйте позже.")
         return
 
-    st.write(f'**Current LABR price:** {st.session_state["latest_labr_price"]:.6f} USD')
+    st.write(f'**Current LABR price:** {latest_labr_price:.6f} USD')
 
     tx_type_choice = st.selectbox('Выберите тип транзакции', ['Продажа', 'Покупка'])
 
     if tx_type_choice == 'Продажа':
         labr_amount = st.number_input('Введите количество LABR:', min_value=1, step=1)
         if st.button('Рассчитать цену после продажи'):
-            new_labr_price = calculate_price_after_selling(st.session_state['wtrx_token_balance'], st.session_state['labr_token_balance'], labr_amount) * st.session_state['wtrx_price_in_usd']
+            new_labr_price = calculate_price_after_selling(wtrx_token_balance, labr_token_balance, labr_amount) * wtrx_price_in_usd
             st.write(f'**Цена после продажи:** {new_labr_price:.6f} USD')
-            st.write(f'Изменение цены: {(new_labr_price - st.session_state["latest_labr_price"]):.6f} USD ({((new_labr_price - st.session_state["latest_labr_price"]) / st.session_state["latest_labr_price"] * 100):.1f}%)')
+            st.write(f'Изменение цены: {(new_labr_price - latest_labr_price):.6f} USD ({((new_labr_price - latest_labr_price) / latest_labr_price * 100):.1f}%)')
 
     elif tx_type_choice == 'Покупка':
         currency_choice = st.selectbox('Выберите валюту', ['USD', 'TRX'])
         if currency_choice == 'USD':
             amount_in_usd = st.number_input('Введите сумму в USD:', min_value=1, step=1)
-            amount_in_wtrx = amount_in_usd / st.session_state['wtrx_price_in_usd']
+            amount_in_wtrx = amount_in_usd / wtrx_price_in_usd
         else:
             amount_in_wtrx = st.number_input('Введите сумму в TRX:', min_value=1, step=1)
 
         if st.button('Рассчитать цену после покупки'):
-            new_labr_price = calculate_price_after_buying(st.session_state['wtrx_token_balance'], st.session_state['labr_token_balance'], amount_in_wtrx) * st.session_state['wtrx_price_in_usd']
+            new_labr_price = calculate_price_after_buying(wtrx_token_balance, labr_token_balance, amount_in_wtrx) * wtrx_price_in_usd
             st.write(f'**Цена после покупки:** {new_labr_price:.6f} USD')
-            st.write(f'Изменение цены: {(new_labr_price - st.session_state["latest_labr_price"]):.6f} USD ({((new_labr_price - st.session_state["latest_labr_price"]) / st.session_state["latest_labr_price"] * 100):.1f}%)')
+            st.write(f'Изменение цены: {(new_labr_price - latest_labr_price):.6f} USD ({((new_labr_price - latest_labr_price) / latest_labr_price * 100):.1f}%)')
 
 if __name__ == '__main__':
     main()
